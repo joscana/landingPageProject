9c3c8699d4a288d763830b1b2d0bdae2f513459a
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-02-24 11:45:51 -0500

    Navbar links to sections on page

--- js/app.js
+++ js/app.js
@@ -1,29 +1,27 @@
-function createListItem() {
-    let my_sections = document.getElementsByClassName("my_section");
-    let navbarList = document.getElementById("navbar__list");
+//Global Variables
 
+//Select sections by class name, select navbar by ID
+
+let my_sections = document.getElementsByClassName("my_section");
+let navbarList = document.getElementById("navbar__list");
+
+
+//Dynamically creates a link for each HTML section
+
+function createNavbarLinks () {
     let count = 1;
-
     for (let section of my_sections) {
         let listItem = document.createElement("li");
-        let newText = document.createTextNode("Section");
+        let a = document.createElement('a');
+        let textNode = document.createTextNode(section.dataset.nav);
+        let link = "#" + section.id;
+        a.appendChild(textNode);
+        a.href= link;
         navbarList.appendChild(listItem);
-        listItem.appendChild(newText);
-
-        console.log(section)
-        let test = createLink(count, "#" + section.id)
+        listItem.appendChild(a)
         count++;
     };
 };
 
 
-function createLink (label, link) {
-    let a = document.createElement('a');
-    let textNode = document.createTextNode(label);
-    a.appendChild(textNode);
-    a.title= label;
-    a.href= link;
-    document.body.appendChild(a);
-};
-
-createListItem();
+createNavbarLinks();
