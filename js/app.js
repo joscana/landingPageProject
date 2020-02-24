a9fadd2b3d488b0ecc70e797f92680ad603fc7e2
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-02-24 07:59:11 -0500

    Javascript dynamic list

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
-window.onload = function () {
-    const sections = [section1, section2, section3, section4];
-    let listItem = document.getElementById(navbar__list);
-    for (let section of sections) {
-        listValue = document.createElement("li");
-        listValue.textContent = section;
-        listItem.appendChild(listValue);
-    }
+function createListItem() {
+    let my_sections = document.getElementsByClassName("my_section");
+    let navbarList = document.getElementById("navbar__list");
+
+    let count = 1;
+
+    for (let section of my_sections) {
+        let listItem = document.createElement("li");
+        let newText = document.createTextNode("Section");
+        navbarList.appendChild(listItem);
+        listItem.appendChild(newText);
+
+        console.log(section)
+        let test = createLink(count, "#" + section.id)
+        count++;
+    };
 };
+
+
+function createLink (label, link) {
+    let a = document.createElement('a');
+    let textNode = document.createTextNode(label);
+    a.appendChild(textNode);
+    a.title= label;
+    a.href= link;
+    document.body.appendChild(a);
+};
+
+createListItem();
